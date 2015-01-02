aee3975a9c3bf598deb45d4ba5f236cc0733730a
slmt <sam123456777@gmail.com>
2015-01-02 19:47:28 +0800

    Create project.

---
+++ src/main/java/slmt/game/bang/App.java
@@ -0,0 +1,12 @@
+package slmt.game.bang;
+
+/**
+ * Hello meow!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Meow!" );
+    }
+}
